3b371fa939a83121cd5fd16737abd4388c9fd4e7
julienPalleau <jpalleau@gmail.com>
2019-11-13 08:42:32 +0100

    this is a git test

--- test.py
+++ test.py
@@ -353,3 +353,5 @@ with open('donnees', 'rb') as fichier:
     mon_depickler = pickle.Unpickler(fichier)
     score_recupere = mon_depickler.load()
     print(score_recupere)
+
+# this is a git test
