39cb2a792a0265b35a1e472683af8601c1970e6d
julienPalleau <jpalleau@gmail.com>
2019-11-27 10:21:43 +0100

    end of chapter Appliquez des methodes de tri en Python

--- ChapitreClass.py
+++ ChapitreClass.py
@@ -1,3 +1,6 @@
+from operator import itemgetter, attrgetter
+
+
 class Personne:
     """Classe definissant une personne caracterisee par :
     - son nom
@@ -123,3 +126,180 @@ tab.ecrire("La forme ?")
 tab.lire()
 tab.effacer()
 tab.lire()
+
+# Les methodes mathematiques
+
+
+class Duree:
+    """Classe contenant des durees sous la forme d'un nombre de minutes et de secondes"""
+
+    def __init__(self, min=0, sec=0):
+        """Constructeur de la classe"""
+        self.min = min  # Nombre de minutes
+        self.sec = sec  # Nombre de secondes
+
+    def __str__(self):
+        """Affichage un peu plus joli de nos objets"""
+        return "{0:02}:{1:02}".format(self.min, self.sec)
+
+    def __add__(self, objet_a_ajouter):
+        """L'objet à ajouter est un entier, le nombre de secondes"""
+        nouvelle_duree = Duree()
+        # On va copier self dans l'objet créé pour avoir la même durée
+        nouvelle_duree.min = self.min
+        nouvelle_duree.sec = self.sec
+        # On ajoute la durée
+        nouvelle_duree.sec += objet_a_ajouter
+
+        # Si le nombre de secondes >= 60
+        if nouvelle_duree.sec >= 60:
+            nouvelle_duree.min += nouvelle_duree.sec // 60
+            nouvelle_duree.sec = nouvelle_duree.sec % 60
+
+        # On renvoie la nouvelle durée
+        return nouvelle_duree
+
+
+d1 = Duree(12, 8)
+print(d1)
+d2 = d1 + 54
+print(d2)
+
+d1 = Duree(3, 5)
+print(d1)
+
+""" Appliquez deux methodes de tri en Python """
+""" Premiere approche du tri """
+prenom = ["Jacques", "Laure", "Andre", "Victoire", "Albert", "Sophie"]
+print("on trie aver sorted")
+print(sorted(prenom))  # sorted renvoir la liste trie
+print(prenom)  # prenom n'a pas ete modifie
+
+print("\n")
+
+print("On trie avec sort")
+print(prenom.sort())  # prenom.sort ne renvoie rien contrairement a sorted(prenom)
+prenom.sort()  # trie la liste
+print(prenom)  # la liste triee est re-affecte dans prenom donc prenom est modifie !
+
+print("\n")
+
+""" Trier avec des cles precises, cela se fait avec une fonction lambda """
+etudiants = [
+    ("Clement", 14, 16),
+    ("Charles", 12, 15),
+    ("Oriane", 14, 18),
+    ("Thomas", 11, 12),
+    ("Damien", 12, 15)
+]
+print("On trie sur sur les moyennes (dernier colonnes) avec une fonction lambda")
+print(sorted(etudiants, key=lambda colonnes: colonnes[2]))
+print("\n")
+
+
+""" Trier une liste d'objets """
+
+
+class Etudiant:
+    """ Classe representant un etudiant.
+
+    On represente un etudiant par son prenom (attribut prenom), son age (attribut age)
+    et sa note moyenne (attribut moyenne, entre 0 et 20)
+
+    Parametres du constructeur :
+        Prenom -- le prenom de l'etudiant
+        age -- l'age de l'etudiant
+        moyenne -- la moyenne de l'etudiant
+    """
+
+    def __init__(self, prenom, age, moyenne):
+        self.prenom = prenom
+        self.age = age
+        self.moyenne = moyenne
+
+    def __repr__(self):
+        return "<Etudiant {} (age={}, moyenne={})>".format(self.prenom, self.age, self.moyenne)
+
+
+etudiants = [
+    Etudiant("Clements", 14, 16),
+    Etudiant("Charles", 12, 15),
+    Etudiant("Oriane", 14, 18),
+    Etudiant("Thomas", 11, 12),
+    Etudiant("Damien", 12, 15),
+]
+
+print("On trie sur une liste d'objets sur la moyenne (dernier colonne)")
+print(sorted(etudiants, key=lambda etudiant: etudiant.moyenne))
+print("\n")
+
+""" Trier dans l'ordre inverse """
+print("On trie sur une liste d'objets sur l'age en ordre decroissant")
+print(sorted(etudiants, key=lambda etudiant: etudiant.age, reverse=True))
+
+print("\n")
+
+""" Plus rapide et plus efficace """
+""" Trier une liste d'objets """
+print("Trie la moyenne avec une liste d'objets avec attrgetter:")
+print(sorted(etudiants, key=attrgetter("moyenne")))
+
+print("\n")
+
+""" Trier selon plusieurs criteres """
+print("trier selon plusieurs criteres ici age et moyenne :")
+print(sorted(etudiants, key=attrgetter("age", "moyenne")))
+
+print("\n")
+
+""" Trier une liste de tuples """
+etudiants = [
+    ("Clements", 14, 16),
+    ("Charles", 12, 15),
+    ("Oriane", 14, 18),
+    ("Thomas", 11, 12),
+    ("Damien", 12, 15),
+]
+# Si on veut trier par moyenne ascendante, nous avons vu qu'il suffisait de faire:
+# sorted(etudiants, key=lambda etudiant: etudiant[2])
+# pour faire la meme chose sans fonction lambda, avec la fonction itemgetter du
+# module operator:
+print("Trie la moyenne via une liste de tuples avec itemgetter :")
+print(sorted(etudiants, key=itemgetter(2)))
+
+print("\n")
+
+""" Chainage de tris """
+
+
+class LigneInventaire:
+
+    """ Classe representant une ligne d'un inventaire de vente.
+
+    Attributs attendus par le constructeur :
+        produit -- le nom du produit
+        prix -- le prix unitaire du produit
+        quantite -- la quantite vendue du produit
+
+    """
+
+    def __init__(self, produit, prix, quantite):
+        self.produit = produit
+        self.prix = prix
+        self.quantite = quantite
+
+    def __repr__(self):
+        return "<ligne d'inventaire {} ({}X{})>".format(self.produit, self.prix, self.quantite)
+
+
+# Creation de l'inventaire
+inventaire = [
+    LigneInventaire("pomme rouge", 1.2, 19),
+    LigneInventaire("orange", 1.4, 24),
+    LigneInventaire("banane", 0.9, 21),
+    LigneInventaire("poire", 1.2, 24),
+]
+
+# On veut trier cette liste par prix et par quantite
+print("Trie de la liste par prix et par quantite:")
+print(sorted(inventaire, key=attrgetter("prix", "quantite")))
