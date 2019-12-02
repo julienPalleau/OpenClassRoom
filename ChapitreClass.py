adea5c198dfb65187a6bde5fc792d717f07f5776
julienPalleau <jpalleau@gmail.com>
2019-12-02 17:40:39 +0100

    TP: Realisez un dictionnaire ordonne

--- ChapitreClass.py
+++ ChapitreClass.py
@@ -392,4 +392,158 @@ ma_chaine = RevStr("Bonjour")
 print(ma_chaine)
 for lettre in ma_chaine:
     print(lettre)
-    print("end")
+
+
+class DictionnaireOrdonnee:
+    """Notre dictionnaire ordonne. L'ordre des donnees est maintenu et il peut donc,
+    contrairement aux dictionnaires usuels, etre trie ou voir de ses donnees inversees """
+
+    def __init__(self, base={}, **donees):
+        """Constructeur de notre objet. Il peut ne prendre aucun parametre
+        (dans ce cas, le dictionnaire sera vide) ou construire un dictionnaire
+        remplis grace :
+        - au dictionnaire 'base' passe en premier parametre;
+        - aux valeurs que l'on retrouve dans 'donnees'."""
+
+        self._cles = []  # liste contenant nos cles
+        self._valeurs = []  # liste contenant les valeurs correspondant à nos cles
+
+        # On verifie que 'base' est un dictionnaire exploitable
+        if type(base) not in (dict, DictionnaireOrdonnee):
+            raise TypeError(
+                "le type attendu est un dictionnaire (usuel ou ordonne)")
+
+        # On recupere les donnees de 'base'
+        for cle in base:
+            self[cle] = base[cle]
+
+        # On recupere les donnees de 'donnees'
+        for cle in donees:
+            self[cle] = donees[cle]
+
+    def __repr__(self):
+        """Representation de notre objet. C'est cette chaine qui sera affichee
+        quand on saisit directement le dictionnaire dans l'interpreteur, ou en
+        utilisant la fonction 'rep'"""
+        chaine = "{"
+        premier_passage = True
+        for cle, valeur in self.items():
+            if not premier_passage:
+                chaine += ", "  # On ajoute la virgule comme separateur
+            else:
+                premier_passage = False
+            chaine += repr(cle) + ":" + repr(valeur)
+        chaine += "}"
+        return chaine
+
+    def __str__(self):
+        """Fonction appelee quand on souhaite afficher le dictionnaire grace
+        à la fonction 'print' ou le convertir en chaine grace au constructeur
+        'str'. On redirige sur __repr__"""
+        return repr(self)
+
+    def __len__(self):
+        """Renvoie la taille du dictionnaire"""
+        return len(self._cles)
+
+    def __contains__(self, cle):
+        """Renvoie True si la cle est dans la liste des cles, False sinon"""
+        return cle in self._cles
+
+    def __getitem__(self, cle):
+        """Renvoie la valeur correspondant à la cle si elle existe, leve
+        une exception KeyError sinon"""
+        if cle not in self._cles:
+            raise KeyError(
+                "La cle {0} ne se trouve pas dans le dictionnaire".format(
+                    cle))
+        else:
+            indice = self._cles.index(cle)
+            return self._valeurs[indice]
+
+    def __setitem__(self, cle, valeur):
+        """ Methode speciale appelee quand on cherche à modifier une cle
+        presente dans le dictionnaire. Si la cle n'est pas presente, on l'ajoute
+        à la fin du dictionnaire"""
+        if cle in self._cles:
+            indice = self._cles.index(cle)
+            self._valeurs[indice] = valeur
+        else:
+            self._cles.append(cle)
+            self._valeurs.append(valeur)
+
+    def __delitem__(self, cle):
+        """Methode appelee quand on souhaite supprimer une cle"""
+        if cle not in self._cles:
+            raise KeyError(
+                "La cle {0} ne se trouve pas dans le dictionnaire".format(
+                    cle))
+        else:
+            indice = self._cles.index(cle)
+            del self._cles[indice]
+            del self._valeurs[indice]
+
+    def __iter__(self):
+        """Methode de parcours de l'objet. On renvoie l'iterateur des cles"""
+        return iter(self._cles)
+
+    def __add__(self, autre_objet):
+        """On renvoie un nouveau dictionnaire contenant les deux
+        dictionnaires mis bout à bout (d'abord self puis autre_objet)"""
+        if type(autre_objet) is type(self):
+            raise TypeError(
+                "Impossible de concatener {0} et {1}".format(
+                    type(self), type(autre_objet)))
+        else:
+            nouveau = DictionnaireOrdonnee()
+
+            # On commence par copier self dans le dictionnaire
+            for cle, valeur in self.items():
+                nouveau[cle] = valeur
+
+            # On copie ensuite autre_objet
+            for cle, valeur in autre_objet.items():
+                nouveau[cle] = valeur
+            return nouveau
+
+    def items(self):
+        """Renvoie un generateur contenant les couples (cles, valeur)"""
+        for i, cle in enumerate(self._cles):
+            valeur = self._valeurs[i]
+            yield(cle, valeur)
+
+    def keys(self):
+        """Cette methode renvoie la liste des cles"""
+        return list(self._cles)
+
+    def values(self):
+        """Cette methode renvoie la liste des valeurs"""
+        return list(self._valeurs)
+
+    def reverse(self):
+        """Inversion du dictionnaire"""
+        # On cree deux listes vides qui contiendront le nouvel ordre des cles
+        # et valeurs
+        cles = []
+        valeurs = []
+        for cle, valeur in self.items():
+            # On ajoute les cles et valeurs au debut de la liste
+            cles.insert(0, cle)
+            valeur.insert(0, valeur)
+        # On met ensuite à jour nos listes
+        self._cles = cles
+        self._valeurs = valeurs
+
+    def sort(self):
+        """Methode permettant de trier le dictionnaire en fonction de ses cles"""
+        # On trie les cles
+        cles_triees = sorted(self._cles)
+        # On cree une liste de valeurs, encore vide
+        valeurs = {}
+        # On parcourt ensuite la liste des cles triees
+        for cle in cles_triees:
+            valeur = self[cle]
+            valeur.append(valeur)
+        # Enfin, on met à jour notre liste de cles et de valeurs
+        self._cles = cles_triees
+        self._valeurs = valeurs
